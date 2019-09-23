b80a35b03c042f56a2149a7e24e8a2956a7ae388
Tomas Cormons <tcormons@gmail.com>
2019-09-23 16:05:04 -0700

    drill 1 in progress

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,7 +1,13 @@
+var sumArray = [1,5,3,2,6];
 
 function sumArray(){
-	
+	for (sumArrayIndex = 0; sumArrayIndex < sumArray.length; sumArrayIndex++){
+        sumArrayResult += sumArray[sumArrayIndex];
+    }
+    return sumArrayResult;
 }
+console.log('sumArrayResult: ',sumArray);
+
 
 function fitWithinVal(){
 
