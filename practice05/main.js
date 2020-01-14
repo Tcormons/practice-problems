2506922bc37d891f3d716f1654b171bf20bddb92
tcormons <tcormons@gmail.com>
2020-01-14 23:35:40 +0000

    completed exercise

---
+++ practice05/main.js
@@ -0,0 +1,17 @@
+function fizzBuzz() {
+  const fizz = "Fizz"
+  const buzz = "Buzz"
+  const fizzbuzz = 'FizzBuzz'
+  let index = 1;
+  while (index < 101) {
+    if (index % 15 === 0) { console.log(fizzbuzz) }
+    else if (index % 3 === 0) { console.log(fizz) }
+    else if (index % 5 === 0) { console.log(buzz) }
+    else {
+      console.log(index)
+    }
+    index++;
+  }
+}
+
+fizzBuzz();
